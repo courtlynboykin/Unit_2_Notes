d72d6ffe98cb5fe847eb270a0f8a42f05d752682
courtlynboykin <courtlynb@google.com>
2023-11-09 10:38:43 -0500

    Use if/else statements to express conditions, Use a when statement for multiple branches

--- app/src/main/java/com/example/unit2notes/MainActivity.kt
+++ app/src/main/java/com/example/unit2notes/MainActivity.kt
@@ -43,6 +43,65 @@ fun main() {
     } else if (trafficLightColor == "Green") {
         println("Go!")
     } else {
-        println("Invalid traffice-light color. Pay attention")
+        println("Invalid traffic-light color. Pay attention")
+    }
+}
+
+//WHEN STATEMENT
+///In Kotlin, when you deal with multiple branches, you can use the when statement instead of the if/else statement because it improves readability
+//WHEN statements are preferred when there are more than two branches to consider
+//A when statement accepts a single value through the parameter. The value is then evaluated against each of the conditions sequentially. The corresponding body of the first condition that's met is then executed. Each condition and body are separated by an arrow (->)
+
+fun main() {
+    val trafficLightColor = "Black"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop!")
+        "Yellow" -> println("Slow!")
+        "Green" -> println("Go!")
+        else -> println("Invalid traffic-light color. Pay attention")
+    }
+}
+
+//Write more complex conditions in a when statement
+//So far you learned how to write when conditions for a single equal condition, such as when the trafficLightColor variable is assigned a "Yellow" value. Next, you learn to use the comma (,), the in keyword, and the is keyword to form more complex when conditions.
+
+fun main() {
+    val x = 8
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x is not a prime number between 1 and 10")
+    }
+}
+
+//Besides the comma (,) symbol to denote multiple conditions, you can also use the in keyword and a range of values in when branches.
+
+//A diagram that shows the anatomy of a when statement. It starts with a when keyword followed by parentheses with a parameter block inside them. Next, inside a pair curly braces, there are two lines of cases. On the first line, there's an in keyword followed by a range start block, two dots, a range end block, an arrow symbol, and then a body block. On the second line, there's a condition block followed by an arrow symbol and a body block.
+
+//To use a range of values, add a number that denotes the start of the range followed by two periods without any spaces and then close it with another number that denotes the end of the range.
+
+//When the value of the parameter is equal to any value in the range between start of the range and the end of the range, the first body executes.
+
+fun main() {
+    val x = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number")
+        else -> println("x is not a prime number between 1 and 10")
+    }
+}
+
+//ou can use the is keyword as a condition to check the data type of an evaluated value.
+
+fun main() {
+    val x: Any = 88.99
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
     }
 }
